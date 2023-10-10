f41042d21497f370e842c935f569a64c3819a182
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-10-10 17:37:24 +0700

    add client

---
+++ client/next.config.js
@@ -0,0 +1,25 @@
+require("dotenv").config({
+    path: ".env",
+});
+
+console.log(process.env.SERVER_URL);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+                port: "",
+                pathname: "**",
+            },
+        ],
+    },
+    env: {
+        SERVER_URL: process.env.SERVER_URL
+    }
+};
+
+module.exports = nextConfig;
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,30 @@
+import AuthWrapper from "@/components/AuthWrapper";
+import Companies from "@/components/Landing/Companies";
+import Everything from "@/components/Landing/Everything";
+import FiverrBusiness from "@/components/Landing/FiverrBusiness";
+import HeroBanner from "@/components/Landing/HeroBanner";
+import JoinFiverr from "@/components/Landing/JoinFiverr";
+import PopularServices from "@/components/Landing/PopularServices";
+import Services from "@/components/Landing/Services";
+import { useStateProvider } from "@/context/StateContext";
+import React from "react";
+
+const index = () => {
+  const [{ showLoginModal, showSignupModel }] = useStateProvider();
+  return (
+    <div>
+      <HeroBanner />
+      <Companies />
+      <PopularServices />
+      <Everything />
+      <Services />
+      <FiverrBusiness />
+      <JoinFiverr />
+      {(showSignupModel || showLoginModal) && (
+        <AuthWrapper type={showLoginModal ? "login" : "signup"} />
+      )}
+    </div>
+  );
+};
+
+export default index;
