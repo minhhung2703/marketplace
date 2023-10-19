addc699c3f3e39daed8896064eb05139e9ef4f1a
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-10-19 19:48:45 +0700

    add route editGig

--- server/routes/GigsRoutes.js
+++ server/routes/GigsRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const {
     addGigs,
     getUserAuthGigs,
-    getGigData
+    getGigData,
+    editGig
 } = require("../controllers/GigsController");
 
 const multer = require("multer");
@@ -13,4 +14,5 @@ const authenticateUsers = require("../middlewares/authentication");
 router.route("/add").post(authenticateUsers, upload.array("images"), addGigs);
 router.route("/get-user-gigs").get(authenticateUsers, getUserAuthGigs);
 router.route("/get-gig-data/:gigId").get(getGigData);
+router.route("/edit-gig/:gigId").put(authenticateUsers, upload.array("images"), editGig)
 module.exports = router;
