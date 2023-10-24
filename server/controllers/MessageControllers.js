580f400fb38e53d8b982e2ae982839dc411205e7
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-10-24 16:28:28 +0700

    Finished getUnReadMessage and markAsRead function

--- server/controllers/MessageControllers.js
+++ server/controllers/MessageControllers.js
@@ -35,64 +35,91 @@ const addMessage = async (req, res) => {
 }
 
 const getMessages = async (req, res) => {
-    if (req.params.orderId && req.user.userId) {
-        const prisma = new PrismaClient();
-        const messages = await prisma.message.findMany({
-            where: {
-                order: {
-                    id: parseInt(req.params.orderId),
+    try {
+        if (req.params.orderId && req.user.userId) {
+            const prisma = new PrismaClient();
+            const messages = await prisma.message.findMany({
+                where: {
+                    order: {
+                        id: parseInt(req.params.orderId),
+                    },
                 },
-            },
-            orderBy: {
-                createdAt: "asc",
-            },
-        });
+                orderBy: {
+                    createdAt: "asc",
+                },
+            });
 
-        await prisma.message.updateMany({
-            where: {
-                orderId: parseInt(req.params.orderId),
-                recipientId: parseInt(req.user.userId),
-            },
-            data: {
-                isRead: true,
-            },
-        });
+            await prisma.message.updateMany({
+                where: {
+                    orderId: parseInt(req.params.orderId),
+                    recipientId: parseInt(req.user.userId),
+                },
+                data: {
+                    isRead: true,
+                },
+            });
 
-        const order = await prisma.orders.findUnique({
-            where: { id: parseInt(req.params.orderId) },
-            include: { gig: true },
-        });
+            const order = await prisma.orders.findUnique({
+                where: { id: parseInt(req.params.orderId) },
+                include: { gig: true },
+            });
 
-        let recipentId;
-        if (order?.buyerId === req.user.userId) {
-            recipentId = order.gig.userId
-        } else if (order?.gig.userId === req.user.userId) {
-            recipentId = order.buyerId;
+            let recipentId;
+            if (order?.buyerId === req.user.userId) {
+                recipentId = order.gig.userId
+            } else if (order?.gig.userId === req.user.userId) {
+                recipentId = order.buyerId;
+            }
+            return res.status(StatusCodes.OK).json({ messages, recipentId })
         }
-        return res.status(StatusCodes.OK).json({ messages, recipentId })
+        throw new CustomError.BadRequestError("Oreder id id required")
+    } catch (error) {
+        console.log(error)
     }
-    throw new CustomError.BadRequestError("Oreder id id required")
 }
 
 const getUnreadMessage = async (req, res, next) => {
-    if (req.user) {
-        const prisma = new PrismaClient();
-        const messages = await prisma.message.findMany({
-            where: {
-                recipient: req.user.userId,
-                isRead: true
-            },
-            include: {
-                sender: true,
-            }
-        });
-        return res.status(StatusCodes.OK).json({ messages })
+    try {
+        if (req.user) {
+            const prisma = new PrismaClient();
+            const messages = await prisma.message.findMany({
+                where: {
+                    recipientId: req.user.userId,
+                    isRead: true
+                },
+                include: {
+                    sender: true,
+                }
+            });
+            return res.status(StatusCodes.OK).json({ messages })
+        }
+        throw new CustomError.BadRequestError("User id is required")
+    } catch (error) {
+        console.log(error)
     }
-    throw new CustomError.BadRequestError("User id is required")
+
+}
+
+const markAsRead = async (req, res) => {
+    try {
+        if (req.user && req.params.messageId) {
+            const prisma = new PrismaClient();
+            const markRead = await prisma.message.update({
+                where: { id: parseInt(req.params.messageId) },
+                data: { isRead: true }
+            });
+            return res.status(StatusCodes.OK).json({ markRead });
+        } else {
+            throw new CustomError.BadRequestError("User Id and message Id is required")
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
     addMessage,
     getMessages,
-    getUnreadMessage
+    getUnreadMessage,
+    markAsRead
 }
--- server/routes/MessagesRoutes.js
+++ server/routes/MessagesRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { addMessage, getMessages, getUnreadMessage } = require("../controllers/MessageControllers");
+const { addMessage, getMessages, getUnreadMessage, markAsRead } = require("../controllers/MessageControllers");
 const authenticateUsers = require("../middlewares/authentication");
 const router = express.Router();
 
 router.route("/add-message/:orderId").post(authenticateUsers, addMessage)
 router.route("/get-message/:orderId").get(authenticateUsers, getMessages)
 router.route("/get-unread-message").get(authenticateUsers, getUnreadMessage)
+router.route("/mark-as-read/:messageId").put(authenticateUsers, markAsRead)
 
 module.exports = router
